78fe17908b3d61d8eca057d4a241a36ea7ddcb2a
Nikolai <82165109+NikolayLeshchev@users.noreply.github.com>
2021-11-25 12:44:20 +0300

    add new class

---
+++ src/Reader.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Reader {
+    public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Введите Ваш возраст");
+        String age = scanner.nextLine();
+    }
+}
